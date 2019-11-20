b75f4c12136198e19a11de67556a1301f2c4f64f
hugforever <woongki2013@gmail.com>
2019-11-20 13:51:25 +0900

    no message

---
+++ BasicJava/app/src/main/java/com/cranberrygame/basicjava/Test.java
@@ -0,0 +1,10 @@
+package com.cranberrygame.basicjava;
+
+public class Test {
+    public static void main(String[] args) {
+
+        //Run Test.main()
+
+        System.out.println("Hello World!");
+    }
+}
